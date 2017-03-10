febeb1d968c16a032d3157951f9b58cc12779573
Paul Hagedorn <panzer1119@gmail.com>
2017-03-10 20:50:47 +0100

    Added an Image Package and an ImageUtils Class
    Added a method that calculates a linear gradient for n steps from one
    color to antoher

---
+++ src/jaddon/image/ImageUtils.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jaddon.image;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Paul
+ */
+public class ImageUtils {
+    
+    public static Color[] getLinearGradient(Color color_start, Color color_stop, int steps) {
+        Color[] colors = new Color[steps];
+        double r_div = ((color_stop.getRed() - color_start.getRed()) / (steps - 1.0));
+        double g_div = ((color_stop.getGreen() - color_start.getGreen()) / (steps - 1.0));
+        double b_div = ((color_stop.getBlue() - color_start.getBlue()) / (steps - 1.0));
+        for(int i = 0; i < steps; i++) {
+            colors[i] = new Color((int) (color_start.getRed() + (i * r_div)), (int) (color_start.getGreen() + (i * g_div)), (int) (color_start.getBlue() + (i * b_div)));
+        }
+        return colors;
+    }
+    
+}
