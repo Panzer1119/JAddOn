84a4ab03bc0944978981a139628a12be4cf5e75a
Paul Hagedorn <panzer1119@gmail.com>
2017-03-19 05:18:34 +0100

    Added support for numbers with decimal places
    Removed old number system constants

--- src/jaddon/math/JNumber.java
+++ src/jaddon/math/JNumber.java
@@ -5,7 +5,9 @@
  */
 package jaddon.math;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
 import java.util.ArrayList;
 
 /**
@@ -15,232 +17,6 @@ import java.util.ArrayList;
 public class JNumber {
     
     /**
-     * UNARY
-     * 0 - 0
-     */
-    public static final int UNARY = 1;
-    
-    /**
-     * BINARY
-     * 0 - 1
-     */
-    public static final int BINARY = 2;
-    
-    /**
-     * TERNARY
-     * 0 - 2
-     */
-    public static final int TERNARY = 3;
-    
-    /**
-     * QUATERNARY
-     * 0 - 3
-     */
-    public static final int QUATERNARY = 4;
-    
-    /**
-     * QUINARY
-     * 0 - 4
-     */
-    public static final int QUINARY = 5;
-    
-    /**
-     * SENARY
-     * 0 - 5
-     */
-    public static final int SENARY = 6;
-    
-    /**
-     * SEPTARY
-     * 0 - 6
-     */
-    public static final int SEPTARY = 7;
-    
-    /**
-     * OCTAL
-     * 0 - 7
-     */
-    public static final int OCTAL = 8;
-    
-    /**
-     * NONAL
-     * 0 - 8
-     */
-    public static final int NONAL = 9;
-    
-    /**
-     * DECIMAL
-     * 0 - 9
-     */
-    public static final int DECIMAL = 10;
-
-    /**
-     * 11 MAL
-     * 0 - A
-     */
-    public static final int SYSTEM11 = 11;
-
-    /**
-     * 12 MAL
-     * 0 - B
-     */
-    public static final int SYSTEM12 = 12;
-
-    /**
-     * 13 MAL
-     * 0 - C
-     */
-    public static final int SYSTEM13 = 13;
-
-    /**
-     * 14 MAL
-     * 0 - D
-     */
-    public static final int SYSTEM14 = 14;
-
-    /**
-     * 15 MAL
-     * 0 - E
-     */
-    public static final int SYSTEM15 = 15;
-
-    /**
-     * 16 MAL
-     * 0 - F
-     */
-    public static final int HEXADECIMAL = 16;
-
-    /**
-     * 17 MAL
-     * 0 - G
-     */
-    public static final int SYSTEM17 = 17;
-
-    /**
-     * 18 MAL
-     * 0 - H
-     */
-    public static final int SYSTEM18 = 18;
-
-    /**
-     * 19 MAL
-     * 0 - I
-     */
-    public static final int SYSTEM19 = 19;
-
-    /**
-     * 20 MAL
-     * 0 - J
-     */
-    public static final int SYSTEM20 = 20;
-
-    /**
-     * 21 MAL
-     * 0 - K
-     */
-    public static final int SYSTEM21 = 21;
-
-    /**
-     * 22 MAL
-     * 0 - L
-     */
-    public static final int SYSTEM22 = 22;
-
-    /**
-     * 23 MAL
-     * 0 - M
-     */
-    public static final int SYSTEM23 = 23;
-
-    /**
-     * 24 MAL
-     * 0 - N
-     */
-    public static final int SYSTEM24 = 24;
-
-    /**
-     * 25 MAL
-     * 0 - O
-     */
-    public static final int SYSTEM25 = 25;
-
-    /**
-     * 26 MAL
-     * 0 - P
-     */
-    public static final int SYSTEM26 = 26;
-
-    /**
-     * 27 MAL
-     * 0 - Q
-     */
-    public static final int SYSTEM27 = 27;
-
-    /**
-     * 28 MAL
-     * 0 - R
-     */
-    public static final int SYSTEM28 = 28;
-
-    /**
-     * 29 MAL
-     * 0 - S
-     */
-    public static final int SYSTEM29 = 29;
-
-    /**
-     * 30 MAL
-     * 0 - T
-     */
-    public static final int SYSTEM30 = 30;
-
-    /**
-     * 31 MAL
-     * 0 - U
-     */
-    public static final int SYSTEM31 = 31;
-
-    /**
-     * 32 MAL
-     * 0 - V
-     */
-    public static final int SYSTEM32 = 32;
-
-    /**
-     * 33 MAL
-     * 0 - W
-     */
-    public static final int SYSTEM33 = 33;
-
-    /**
-     * 34 MAL
-     * 0 - X
-     */
-    public static final int SYSTEM34 = 34;
-
-    /**
-     * 35 MAL
-     * 0 - Y
-     */
-    public static final int SYSTEM35 = 35;
-
-    /**
-     * 36 MAL
-     * 0 - Z
-     */
-    public static final int SYSTEM36 = 36;
-    
-    /**
-     * All normal numbers from 0 to 9
-     */
-    public static final String NUMBERSNORMAL = "0123456789";
-    
-    /**
-     * All normal numbers from 0 to 9 and all advanced numbers from A to Z
-     */
-    public static final String NUMBERSADVANCED = NUMBERSNORMAL + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-    /**
      * Actual number
      */
     private String number = "";
@@ -248,85 +24,144 @@ public class JNumber {
     /**
      * Actual number system
      */
-    private int number_system = DECIMAL;
+    private NumberSystem number_system = NumberSystem.DECIMAL;
+    
+    
+    /**
+     * Start position of the repeating pattern (-1 for no period)
+     */
+    private int period = -1;
+    
+    /**
+     * English Number splitter
+     */
+    public static final String DOT = ".";
+    
+    /**
+     * European Number splitter
+     */
+    public static final String COMMA = ",";
     
     /**
      * Basic Constructor
      */
     public JNumber() {
         number = "0";
-        number_system = DECIMAL;
+        number_system = NumberSystem.DECIMAL;
     }
     
     /**
      * Standard Constructor
      * @param number String Number
-     * @param number_system Integer Number system
+     * @param number_system NumberSystem Number system
      */
-    public JNumber(String number, int number_system) {
+    public JNumber(String number, NumberSystem number_system) {
         setNumber(number);
         setNumberSystem(number_system);
     }
     
     /**
      * Converts the actual number to another number system
-     * @param number_system Integer Number system
+     * @param number_system NumberSystem Number system
      * @return Boolean True if it worked False if not
      */
-    public boolean convertTo(Integer number_system) {
+    public final JNumber convertTo(NumberSystem number_system) {
         if(!isValidNumberSystem(number_system)) {
-            return false;
+            return null;
         }
         JNumber number_c = convertFromTo(this, number_system);
         if(number_c != null) {
-            this.number = number_c.number;
-            this.number_system = number_c.number_system;
-            return true;
+            return number_c;
         } else {
-            return false;
+            return null;
         }
     }
     
     /**
      * Converts the number to another number system
      * @param number JNumber Number to convert
-     * @param number_system Integer Number system to convert to
+     * @param number_system NumberSystem Number system to convert to
      * @return Boolean True if it worked False if not
      */
-    public static JNumber convertFromTo(JNumber number, int number_system) {
+    public static final JNumber convertFromTo(JNumber number, NumberSystem number_system) {
         if(!isValidNumberSystem(number_system)) {
             return null;
         }
-        final BigInteger number_system_big = new BigInteger("" + number_system);
-        String number_old = number.number;
-        int number_system_old = number.number_system;
-        BigInteger number_complete = BigInteger.ZERO;
-        for(int i = 0; i < number_old.length(); i++) {
-            int value = getNumberValue("" + number_old.charAt(i));
-            number_complete = number_complete.add(new BigInteger("" + value).multiply((new BigInteger("" + number_system_old).pow(number_old.length() - i - 1))));
+        final BigDecimal number_system_big = new BigDecimal("" + number_system.getSystem());
+        final BigDecimal number_system_big_old = new BigDecimal("" + number.getNumberSystem().getSystem());
+        String number_old = number.getNumber();
+        BigDecimal number_complete = BigDecimal.ZERO;
+        String point = DOT;
+        int index_point = number_old.indexOf(point);
+        if(index_point == -1) {
+            point = COMMA;
+            index_point = number_old.indexOf(point);
         }
-        ArrayList<Integer> numbers_new = new ArrayList<>();
-        if(number_system != UNARY) {
+        for(int i = (number_old.length() - 1); i >= 0; i--) {
+            int i_ = i;
+            if((number_old.length() - i - 1) == index_point) {
+                continue;
+            } else if((number_old.length() - i - 1) > index_point) {
+                i_ += 1;
+            }
+            final int value = getNumberValue("" + number_old.charAt(number_old.length() - i - 1));
+            final int exponent = i_ - ((index_point != -1) ? (number_old.length() - index_point) : 0);
+            BigDecimal add = null;
+            if(exponent >= 0) {
+                add = BigDecimal.valueOf(value).multiply(number_system_big_old.pow(exponent));
+            } else {
+                add = BigDecimal.valueOf(value).divide(number_system_big_old.pow(-1 * exponent));
+            }
+            number_complete = number_complete.add(add);
+        }
+        BigInteger number_complete_pre_comma = number_complete.toBigInteger();
+        BigDecimal number_complete_post_comma = number_complete.subtract(new BigDecimal(number_complete_pre_comma));
+        final ArrayList<Integer> numbers_new = new ArrayList<>();
+        final ArrayList<Integer> numbers_new_comma = new ArrayList<>();
+        final ArrayList<BigDecimal> numbers_loop = new ArrayList<>();
+        int loop_begin = -1;
+        if(number_system != NumberSystem.UNARY) {
             boolean done = false;
+            int i = 0;
             while(!done) {
-                BigInteger rest = number_complete.mod(number_system_big);
-                numbers_new.add(rest.intValueExact());
-                BigInteger next = number_complete.divide(number_system_big);
-                number_complete = next;
-                if(number_complete.equals(BigInteger.ZERO)) {
+                if((index_point != -1 && i < index_point) || index_point == -1) {
+                    final BigInteger rest = number_complete_pre_comma.mod(number_system_big.toBigInteger());
+                    numbers_new.add(rest.intValue());
+                    number_complete_pre_comma = number_complete_pre_comma.divide(number_system_big.toBigInteger());
+                } else {
+                    number_complete_post_comma = number_complete_post_comma.multiply(number_system_big);
+                    if(!numbers_loop.contains(number_complete_post_comma)) {
+                        numbers_loop.add(number_complete_post_comma);
+                    } else {
+                        loop_begin = numbers_loop.indexOf(number_complete_post_comma);
+                        numbers_loop.clear();
+                        done = true;
+                        break;
+                    }
+                    numbers_new_comma.add(number_complete_post_comma.intValue());
+                    number_complete_post_comma = number_complete_post_comma.remainder(BigDecimal.ONE, MathContext.UNLIMITED);
+                }
+                if((number_complete_pre_comma.compareTo(BigInteger.ZERO) == 0) && (number_complete_post_comma.compareTo(BigDecimal.ZERO) == 0)) {
                     done = true;
                 }
+                i++;
             }
         } else {
-            for(int i = 0; i < number_complete.intValueExact(); i++) {
-                numbers_new.add(0);
+            for(int i = 0; i < number_complete.intValue(); i++) {
+                numbers_new.add(1);
             }
         }
         String number_end = "";
         for(int i = 0; i < numbers_new.size(); i++) {
             number_end += getNumberSymbol(numbers_new.get(numbers_new.size() - i - 1));
         }
-        JNumber number_new = new JNumber(number_end, number_system);
+        if(!numbers_new_comma.isEmpty()) {
+            number_end += point;
+            for(int i = 0; i < numbers_new_comma.size(); i++) {
+                number_end += getNumberSymbol(numbers_new_comma.get(i));
+            }
+        }
+        JNumber number_new = new JNumber(number_end, number_system).setPeriodStart(loop_begin);
         return number_new;
     }
     
@@ -335,8 +170,8 @@ public class JNumber {
      * @param number Integer Number
      * @return String Number
      */
-    public static String getNumberSymbol(int number) {
-        return "" + NUMBERSADVANCED.charAt(number);
+    public static final String getNumberSymbol(int number) {
+        return "" + NumberSystem.NUMBERSADVANCED.charAt(number);
     }
     
     /**
@@ -344,15 +179,18 @@ public class JNumber {
      * @param number String Number
      * @return Integer Number
      */
-    public static int getNumberValue(String number) {
-        return NUMBERSADVANCED.indexOf(number);
+    public static final int getNumberValue(String number) {
+        if(number.equals(DOT) || number.equals(COMMA)) {
+            return -1;
+        }
+        return NumberSystem.NUMBERSADVANCED.indexOf(number);
     }
 
     /**
      * Returns the actual number
      * @return String Number
      */
-    public String getNumber() {
+    public final String getNumber() {
         return number;
     }
 
@@ -360,42 +198,93 @@ public class JNumber {
      * Sets the actual bumber
      * @param number String Number
      */
-    public void setNumber(String number) {
+    public final JNumber setNumber(String number) {
         this.number = number;
+        return this;
     }
 
     /**
      * Returns the actual number system
-     * @return Integer Number system
+     * @return NumberSystem Number system
      */
-    public int getNumberSystem() {
+    public final NumberSystem getNumberSystem() {
         return number_system;
     }
 
     /**
      * Sets the actual number system
-     * @param number_system Integer Number system
+     * @param number_system NumberSystem Number system
      */
-    public void setNumberSystem(int number_system) {
+    public final JNumber setNumberSystem(NumberSystem number_system) {
         if(isValidNumberSystem(number_system)) {
             this.number_system = number_system;
         } else {
-            this.number_system = DECIMAL;
+            this.number_system = NumberSystem.DECIMAL;
         }
+        return this;
+    }
+    
+    /**
+     * Returns the start of a repeating pattern (-1 for no one)
+     * @return Integer Period begin
+     */
+    public final int getPeriodStart() {
+        return period;
+    }
+    
+    /**
+     * Sets the start of a repeating pattern (-1 for no one)
+     * @param period Integer Period begin
+     * @return JNumber This object
+     */
+    private final JNumber setPeriodStart(int period) {
+        this.period = period;
+        return this;
+    }
+    
+    /**
+     * Returns the index of the point
+     * @return Integer Index of point
+     */
+    public final int indexOfPoint() {
+        int index = number.indexOf(DOT);
+        if(index == -1) {
+            index = number.indexOf(COMMA);
+        }
+        return index;
     }
     
     /**
      * Returns if a number system exists
-     * @param number_system Integer Number system
+     * @param number_system NumberSystem Number system
      * @return Boolean True if it is a valid number system False if not
      */
-    public static boolean isValidNumberSystem(int number_system) {
-        return (number_system > 0) && (number_system <= SYSTEM36);
+    public static final boolean isValidNumberSystem(NumberSystem number_system) {
+        return number_system != null;
     }
     
     @Override
-    public String toString() {
-        return "Number: \"" + number + "\", Number System: " + number_system;
+    public final String toString() {
+        return String.format("Number: \"%s\", Number System: %s", number, number_system);
+    }
+    
+    public final String toStringFormatted(String point) {
+        if(period != -1) {
+            String temp = "";
+            for(int i = 0; i < number.length(); i++) {
+                String temp_ = "" + number.charAt(i);
+                if(i > (indexOfPoint() + period)) {
+                    temp += "\u0305";
+                }
+                if(temp_.equals(DOT) || temp_.equals(COMMA)) {
+                    temp_ = point;
+                }
+                temp += temp_;
+            }
+            return String.format("Number: \"%s\", Number System: %s", temp, number_system);
+        } else {
+            return toString();
+        }
     }
     
 }
