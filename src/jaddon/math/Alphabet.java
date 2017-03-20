8cc279bc7b97920d02b41c7c8c026ba5c74bdbc5
Paul Hagedorn <panzer1119@gmail.com>
2017-03-20 18:17:01 +0100

    Added Alphabet Object
    This Object extends an HashMap to manage an Alphabet

---
+++ src/jaddon/math/Alphabet.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jaddon.math;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Paul
+ */
+public class Alphabet extends HashMap<String, Integer> implements Serializable {
+    
+    public Alphabet() {
+        super();
+    }
+    
+    public Alphabet(Object[][]... numbers) {
+        super();
+        put(numbers);
+    }
+    
+    public final Alphabet put(Object[][]... numbers) {
+        for(Object[] number : numbers) {
+            if(number.length != 2) {
+                continue;
+            }
+            if(number[0] instanceof String && number[1] instanceof Integer) {
+                put((String) number[0], (Integer) number[1]);
+            } else if(number[0] instanceof Integer && number[1] instanceof String) {
+                put((String) number[1], (Integer) number[0]);
+            }
+        }
+        return this;
+    }
+    
+    public final boolean isValid() {
+        return isPatternValid() && isNumbersValid();
+    }
+    
+    public final boolean isNumbersValid() {
+        final ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i : values()) {
+            if(!numbers.contains(i)) {
+                numbers.add(i);
+            }
+        }
+        numbers.sort((Integer o1, Integer o2) -> o1 - o2);
+        int n = 0;
+        for(int i : numbers) {
+            if(i != n) {
+                return false;
+            }
+            n++;
+        }
+        return true;
+    }
+    
+    public final boolean isPatternValid() {
+        for(String g : keySet()) {
+            for(char c : g.toCharArray()) {
+                for(String g_ : keySet()) {
+                    if(g.equals(g_)) {
+                        continue;
+                    }
+                    if(g_.contains("" + c)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+}
